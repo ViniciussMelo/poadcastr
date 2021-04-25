b58b57e0c566c3816e285fb31b8a7624e9315577
Vinicius Spada Melo <vinicius.smelo54@gmail.com>
2021-04-25 12:50:19 -0300

    feat: class 04, ref audio and context api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,13 @@
+import { useContext } from 'react';
 import { GetStaticProps } from 'next';
 import Image from 'next/image';
 import Link from 'next/link';
 import { format, parseISO } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR';
+
 import { api } from '../services/api';
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
+import { PlayerContext } from '../contexts/PlayerContext';
 
 import styles from './home.module.scss';
 
@@ -25,9 +28,13 @@ type HomeProps = {
 }
 
 export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
+  const { play } = useContext(PlayerContext);
+
   return (
     <div className={styles.homepage}>
       <section className={styles.latestEpisodes}>
+        <h2>Últimos lançamentos</h2>
+
         <ul>
           {latestEpisodes.map(episode => (
             <li key={episode.id}>
@@ -48,7 +55,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                 <span>{episode.durationAsString}</span>
               </div>
 
-              <button type="button">
+              <button type="button" onClick={() => play(episode)}>
                 <Image width={192} height={192} src="/play-green.svg" alt="Tocar episódio" />
               </button>
             </li>
@@ -91,7 +98,7 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
                 <td style={{ width: 100 }}>{episode.publishedAt}</td>
                 <td>{episode.durationAsString}</td>
                 <td>
-                  <button type="button">
+                  <button type="button" onClick={() => play(episode)}>
                     <img src="/play-green.svg" alt="Tocar episósido" />
                   </button>
                 </td>
